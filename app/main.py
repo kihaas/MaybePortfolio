43bb10cd68364ac10839be29aab74de07c2f78ff
kihaas <qwkihaas@gmail.com>
2025-08-21 21:45:33 +0400

    skelet

--- app/main.py
+++ app/main.py
@@ -15,7 +15,7 @@ def read_root():
     return {"Hello": "World"}
 
 if __name__ == "__main__":
-    # Здесь нужно указать модуль и объект приложения в формате "module:app"
+    # Здесь нужно указать модульь и объект приложения в формате "module:app"
     uvicorn.run("app.main:app", host="127.0.0.1", port=8000, reload=True)
 
 
